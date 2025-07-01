407943931e11642f63f0945fd74fe436a8a957c6
Clara Scherer <clara@modugen.de>
2025-07-01 09:42:04 +0200

    Fix error caused by Meta being used before react router was set up properly

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,6 @@
 import {
   isRouteErrorResponse,
   Links,
-  Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -44,7 +43,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
         <Links />
       </head>
       <body>
